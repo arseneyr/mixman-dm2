372a5d53fefc9538d411acd27bb0fdf6c9fca6c9
arseneyr <arseneyr@users.noreply.github.com>
2018-09-13 18:38:05 -0700

    Add project files.

---
+++ Mixman_DM2/Public.h
@@ -0,0 +1,24 @@
+/*++
+
+Module Name:
+
+    public.h
+
+Abstract:
+
+    This module contains the common declarations shared by driver
+    and user applications.
+
+Environment:
+
+    user and kernel
+
+--*/
+
+//
+// Define an Interface Guid so that app can find the device and talk to it.
+//
+
+DEFINE_GUID (GUID_DEVINTERFACE_MixmanDM2,
+    0x2d83f9e0,0x703e,0x49fa,0xb2,0xab,0x2b,0xbc,0x98,0x5a,0x94,0x30);
+// {2d83f9e0-703e-49fa-b2ab-2bbc985a9430}
