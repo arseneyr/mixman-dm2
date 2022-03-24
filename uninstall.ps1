aab0c2647cf18053173c1b2551ab53426a4c04eb
arseneyr <10376473+arseneyr@users.noreply.github.com>
2022-03-24 09:56:05 -0700

    fixing uninstall script

--- uninstall.ps1
+++ uninstall.ps1
@@ -1,19 +1,16 @@
 $output = (Invoke-Expression '.\devcon.exe dp_enum') | out-string
 
-if ($output -match "(?m)^(?:(oem\d+\.inf)\r\n +Provider: Fabrikam Inc\.\r\n +Class: Extensions\r\n)?(oem\d+\.inf)\r\n +Provider: Fabrikam Inc\.\r\n +Class: Sound, video and game controllers") {
+$filter = $output | select-string -Pattern "(?m)^(oem\d+\.inf)\r\n +Provider: Fabrikam Inc\.\r\n +Class: Extensions" -AllMatches | select -ExpandProperty Matches | % {($_.Groups)[1].value}
+$midi = $output | select-string -Pattern "(?m)^(oem\d+\.inf)\r\n +Provider: Fabrikam Inc\.\r\n +Class: Sound, video and game controllers" -AllMatches | select -ExpandProperty Matches | % {($_.Groups)[1].value}
 
-  $drivers = @()
-
-  $matches.Keys | ForEach-Object { if ($_ -ne '0') {
-      $drivers += $matches[$_]
-    } }
+if ($filter -or $midi) {
 
   if ((Invoke-Expression ".\devcon.exe stack ''USB\ROOT_HUB30" | out-string) -match "Mixman_Filter") {
     Invoke-Expression ".\devcon.exe remove ''USB\ROOT_HUB30"
     $rescan = $true
   }
 
-  $drivers | ForEach-Object { Invoke-Expression ".\devcon.exe -f dp_delete $_" }
+  $filter + $midi | ForEach-Object { Invoke-Expression ".\devcon.exe -f dp_delete $_" }
 
   if ($rescan) {
     Invoke-Expression ".\devcon.exe rescan" 
